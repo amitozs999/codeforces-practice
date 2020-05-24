d0b7368302c95a186f3cb96caf09c52a7802363d
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-05-24 23:51:11 +0530

    watermelon

---
+++ watermelon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int i;
+    cin>>i;
+    if(i%2==0 && i!=2 && i!=0){
+        cout<<"YES";
+        
+    }else{
+        cout<<"NO";
+    }
+    
+}
