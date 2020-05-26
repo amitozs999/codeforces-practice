b1d56703d93f638dd50792926137d8dce3c85938
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-05-26 18:47:37 +0530

    Drinks

---
+++ Drinks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    double sum=0.0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+       sum+=arr[i];
+        
+    }
+    cout<<sum/n;
+    
+    
+}
