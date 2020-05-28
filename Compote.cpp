2c6603a2db49d095415ed943323819aa5b14b658
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-05-29 00:58:22 +0530

    Compote

---
+++ Compote.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+  long long int a,b,c;
+  cin>>a>>b>>c;
+  long long int sum=0;
+  long long int m=min(a,b/2);
+  m=min(m,c/4);
+  cout<<m*7;
+  
+    
+}
