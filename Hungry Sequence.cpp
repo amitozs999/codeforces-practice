43110e18326926a33aafb7f76e90425d8647a5db
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-05-30 00:26:06 +0530

    Hungry Sequence

---
+++ Hungry Sequence.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        
+        cout<<3*n+i<<" ";
+    }
+    
+}
